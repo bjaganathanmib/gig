4ca0dd5410a5781b02ef18fb832b7b519609f564
jaganathan <bjaganathanmib@gmail.com>
2021-05-19 20:39:01 -0400

    issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  family = 'God is really Great';
+  family = 'God is really really Great';
 }
